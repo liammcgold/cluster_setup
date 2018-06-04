45d116bab530df09477da0eee91fb5bb2b0d0903
user1 <ljm0015@mix.wvu.edu>
2018-06-04 12:33:17 -0400

    auto

--- setup.sh
+++ setup.sh
@@ -3,9 +3,9 @@
 
 wget -P ./ "https://repo.anaconda.com/archive/Anaconda3-5.2.0-Linux-x86_64.sh"
 
-yes | bash Anaconda3-5.2.0-Linux-x86_64.sh
+ yes |echo| echo| echo|  echo| echo| echo| echo|  echo| echo| echo| echo|  echo| echo| echo| echo|  echo|  echo| echo| echo|  echo| echo| echo| echo|  echo| echo| echo| echo|  echo| echo| echo| echo|  echo|echo| echo| echo|  echo| echo| echo| echo|  echo| echo| echo| echo|  echo| echo| echo| echo|  echo| echo| echo| echo|  echo| echo| echo| echo|  echo| echo| echo| echo|  echo| echo| echo| echo|  echo|yes | bash Anaconda3-5.2.0-Linux-x86_64.sh
 
 
 
 
-#bash Anaconda3-5.2.0-Linux-x86_64.sh | yes | | | |  |  |  | |  |  |  | |  |  |  | |  |  |  | |  |  |  | |  |  |  | |  |  |  | |  |  |  | |  |  |  | |  |  |  | |  |  |  | |  |  |  | |  |  |  | |  |  |  | yes |
+#bash Anaconda3-5.2.0-Linux-x86_64.sh | yes | echo| echo| echo|  echo|  |  | |  |  |  | |  |  |  | |  |  |  | |  |  |  | |  |  |  | |  |  |  | |  |  |  | |  |  |  | |  |  |  | |  |  |  | |  |  |  | |  |  |  | |  |  |  | yes |
