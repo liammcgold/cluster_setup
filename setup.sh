6c827d8f8ed767d6868f3665522465712f609153
user1 <ljm0015@mix.wvu.edu>
2018-06-04 12:20:40 -0400

    auto

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 
-wget "https://repo.anaconda.com/archive/Anaconda3-5.2.0-Linux-x86_64.sh"
+wget -P ./ "https://repo.anaconda.com/archive/Anaconda3-5.2.0-Linux-x86_64.sh"
 
-bash Anaconda-latest-Linux-x86_64.sh  
+bash Anaconda3-5.2.0-Linux-x86_64.sh
